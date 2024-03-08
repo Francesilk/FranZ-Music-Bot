d451069495de7f91c2e8acc5e03aef986166b75c
Francesilk <162698273+Francesilk@users.noreply.github.com>
2024-03-08 19:00:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  TOKEN: "ODgyMTgxNjkzNzY5OTEyMzIw.GFn7Ee.IeAXvnmppRpOYmxkZcWWfLFjH5e7sjI2TPnYNo",
+  ownerID: ["882181693769912320", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
